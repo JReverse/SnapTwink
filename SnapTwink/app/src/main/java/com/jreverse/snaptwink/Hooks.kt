ec72eff583fa2d37a6f91ce97d824d9df48a3453
J <40630518+JReverse@users.noreply.github.com>
2021-09-09 01:27:47 -0500

    Start Working on SS Bypass

---
+++ SnapTwink/app/src/main/java/com/jreverse/snaptwink/Hooks.kt
@@ -0,0 +1,42 @@
+package com.jreverse.snaptwink
+
+import android.content.Context
+import android.widget.Toast
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers.findAndHookMethod
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Hooks : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (!lpparam.packageName.equals("com.snapchat.android"))
+            return;
+
+        XposedBridge.log("[SnapTwink] Hooking Snapchat")
+        findAndHookMethod(
+            "android.app.Application",
+            lpparam.classLoader,
+            "attach",
+            Context::class.java,
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val snapContext = param.args[0] as Context
+                    Toast.makeText(snapContext, "SnapTwink Initialized", Toast.LENGTH_SHORT).show();
+                    XposedBridge.log("[SnapTwink] Hooked Snapchat")
+                    super.afterHookedMethod(param)
+                    findAndHookMethod(
+                        "Fq6",
+                        lpparam.classLoader,
+                        "a",
+                        "fq6",
+                        "eq6",
+                        XC_MethodReplacement.DO_NOTHING
+                    ); // Screenshot Bypass
+                    XposedBridge.log("[SnapTwink] Screenshot Bypassed")
+                }
+            })
+    }
+}
