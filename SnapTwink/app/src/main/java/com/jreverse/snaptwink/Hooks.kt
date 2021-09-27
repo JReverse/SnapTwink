c6a5990959e93dbee8e7b5a7991ed1c5e92f66de
J <40630518+JReverse@users.noreply.github.com>
2021-09-27 02:27:13 -0500

    Update SS Bypass + Saving

--- SnapTwink/app/src/main/java/com/jreverse/snaptwink/Hooks.kt
+++ SnapTwink/app/src/main/java/com/jreverse/snaptwink/Hooks.kt
@@ -3,12 +3,80 @@ package com.jreverse.snaptwink
 import android.content.Context
 import android.widget.Toast
 import de.robv.android.xposed.*
-import de.robv.android.xposed.XposedHelpers.findAndHookConstructor
 import de.robv.android.xposed.XposedHelpers.findAndHookMethod
 import de.robv.android.xposed.callbacks.XC_LoadPackage
-import java.util.concurrent.TimeUnit
+import de.robv.android.xposed.XposedBridge
+
+import android.os.Environment
+import java.io.*
+import java.lang.Exception
+import java.lang.StringBuilder
+import kotlin.experimental.and
+
 
 class Hooks : IXposedHookLoadPackage {
+    fun copySnapMedia(fromFile: File, homeDir: File): Boolean {
+        return try {
+            val hexCode = getHex(fromFile)
+            val extension: String = when {
+                hexCode.contains("ff d8 ff e0 00 10") -> {
+                    ".jpg"
+                }
+                hexCode.contains("89 50 4e 47 0d 0a") -> {
+                    ".png"
+                }
+                checkWildcard(hexCode, "00 00 00 ** 66 74", '*') -> {
+                    ".mp4"
+                }
+                checkWildcard(hexCode, "52 49 46 46 ** **", '*') -> {
+                    ".webp"
+                }
+                else -> {
+                    XposedBridge.log("[SnapTwink]: File " + fromFile.name + " is unknown")
+                    return false
+                }
+            }
+            XposedBridge.log("[SnapTwink]: File " + fromFile.name + " is a " + extension)
+            val inputStream: InputStream = FileInputStream(fromFile)
+            val outputStream: OutputStream = FileOutputStream(homeDir.toString() + "/" + fromFile.name + extension)
+            val buf = ByteArray(1024)
+            var len: Int
+            while (inputStream.read(buf).also { len = it } > 0) {
+                outputStream.write(buf, 0, len)
+            }
+            inputStream.close()
+            outputStream.close()
+            true
+        } catch (e: Exception) {
+            XposedBridge.log("[SnapTwink]: ERROR " + e.message)
+            false
+        }
+    }
+
+    private fun getHex(file: File): String {
+        val builder = StringBuilder()
+        try {
+            val fin = FileInputStream(file.path)
+            val buffer = ByteArray(1024)
+            var bytesRead: Int
+            while (fin.read(buffer).also { bytesRead = it } > -1) for (`in` in 0 until bytesRead) builder.append(String.format("%02x", buffer[`in`] and 0xFF.toByte())).append(if (`in` != bytesRead - 1) " " else "")
+        } catch (e: IOException) {
+            XposedBridge.log("[SnapTwink]: ERROR " + e.message)
+        }
+        return builder.substring(0, 17)
+    }
+
+    private fun checkWildcard(s1: String, s2: String, wildCard: Char): Boolean {
+        if (s1.length != s2.length) return false
+        for (i in s1.indices) {
+            val c1 = s1[i]
+            val c2 = s2[i]
+            if (c1 != wildCard && c2 != wildCard && c1 != c2) {
+                return false
+            }
+        }
+        return true
+    }
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (!lpparam.packageName.equals("com.snapchat.android"))
             return
@@ -27,55 +95,50 @@ class Hooks : IXposedHookLoadPackage {
                     XposedBridge.log("[SnapTwink] Hooked Snapchat")
                     super.afterHookedMethod(param)
                     findAndHookMethod(
-                        "QU7",
+                        "le8",
                         lpparam.classLoader,
                         "b",
-                        "PU7",
+                        "ke8",
                         XC_MethodReplacement.DO_NOTHING
                     ) // Screenshot Bypass
-                    XposedBridge.log("[SnapTwink] Screenshot Bypassed")
-                    /*
-                    findAndHookConstructor(
-                        "Pqg",
-                        lpparam.classLoader,
-                        Long::class.java,
-                        TimeUnit::class.java,
-                        Int::class.java,
-                        object : XC_MethodHook() {
-                            @Throws(Throwable::class)
-                            override fun beforeHookedMethod(param: MethodHookParam) {
-                                super.beforeHookedMethod(param)
-                                if(param.args[0].equals(60000)){
-                                        XposedHelpers.setLongField(param.thisObject, "c", 2147483647L)
-                                        param.args[0] = 2147483647L
-                                }
-                            }
-                        }) // Max Video Length Chat Bypass */
-                    /*
-                    TODO AD BLOCK
-                    findAndHookConstructor(
-                        "fe9",
-                        lpparam.classLoader,
-                        String::class.java,
-                        String::class.java,
-                        Boolean::class.java,
-                        Boolean::class.java,
-                        object : XC_MethodHook() {
-                            @Throws(Throwable::class)
-                            override fun beforeHookedMethod(param: MethodHookParam) {
-                                super.beforeHookedMethod(param)
-                                if(param.args[0].equals("ADS_HOLDOUT_01")){
-                                    if(param.args[2].equals("ADS_IN_AA")) {
-                                        XposedHelpers.setBooleanField(param.thisObject, "c", false)
-                                        XposedHelpers.setBooleanField(param.thisObject, "d", false)
-                                        param.args[3] = false
-                                        param.args[4] = false
-                                    }
-                                }
+                    val home = File(String.format("%s/SnapTwink/", Environment.getExternalStorageDirectory()))
+                    if (!home.exists()) {
+                        XposedBridge.log("[SnapTwink]: Home directory not found, creating one...")
+                        home.mkdir()
+                    }
+
+                    XposedBridge.log("[SnapTwink]: Checking home folder permissions: READ = " + home.canRead().toString() + ", WRITE = " + home.canWrite())
+                    if (!home.canRead() || !home.canWrite()) {
+                        home.setReadable(true)
+                        home.setWritable(true)
+                    }
+
+                    val snapStorage = File(String.format("%s/files/file_manager/chat_snap/", snapContext.applicationInfo.dataDir))
+
+                    if (snapStorage.exists()) {
+                        XposedBridge.log("[SnapTwink]: Snapchat 'chat_snap' folder exists")
+                        XposedBridge.log("[SnapTwink]: Checking folder permissions: READ = " + snapStorage.canRead().toString() + ", WRITE = " + snapStorage.canWrite())
+                        if (!snapStorage.canRead()) {
+                            XposedBridge.log("[SnapTwink]: Trying to get read access...")
+                            if (snapStorage.setReadable(true)) {
+                                XposedBridge.log("[SnapTwink]: Read access was successful")
+                            } else {
+                                XposedBridge.log("[SnapTwink]: Failed to gain read access")
                             }
                         }
-                    ) // Ad Bypass */
-                    //XposedBridge.log("[SnapTwink] Ads Bypassed")
+                        val files: Array<File> = snapStorage.listFiles()
+                        XposedBridge.log("[SnapTwink]: Snapchat 'chat_snap' folder have a length of: " + files.size)
+                        for (i in files.indices) {
+                            XposedBridge.log("[SnapTwink]: Found file: " + files[i].getPath())
+                            if (copySnapMedia(files[i], home)) {
+                                XposedBridge.log("[SnapTwink]: Successfully saved Snap: " + files[i].getPath())
+                            } else {
+                                XposedBridge.log("[SnapTwink]: Failed to save Snap: " + files[i].getPath())
+                            }
+                        }
+                    } else {
+                        XposedBridge.log("[SnapTwink]: Snapchat 'chat_snap' folder does NOT exists")
+                    }
                 }
             })
             XposedBridge.log("[SnapTwink] Hooks completed")
